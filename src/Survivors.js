446e0afc0a8f59e757fe808e7ee695abd8fab1dc
sergiomurilovarela <sergiomurilovarela@gmail.com>
2017-03-17 22:27:01 -0300

    Add id property to each state.survivors object

--- src/Survivors.js
+++ src/Survivors.js
@@ -1,6 +1,37 @@
 import React from 'react';
 
 class Survivors extends React.Component {
+
+  render(){
+    return(
+      <ul className="survivors-table">
+        <SurvivorsTable />
+      </ul>
+    );
+  }
+}
+
+function getSurvivors(e){
+  return fetch('http://zssn-backend-example.herokuapp.com/api/people.json')
+  .then((response) => response.json())
+  .then((survivors) => {
+    e.setState({survivors})
+  });
+}
+
+function getSurvivorsId(e){
+  let id;
+  let survivors = e.state.survivors;
+  let count = 0;
+  e.state.survivors.map((survivor) => {
+    id = survivor["location"].split("people/")[1];
+    survivors[count]["id"] = id;
+    count += 1;
+    return survivors;
+  });
+}
+
+class SurvivorsTable extends React.Component{
   constructor(){
     super();
     this.state = {survivors: []}
@@ -8,64 +39,55 @@ class Survivors extends React.Component {
 
   componentWillMount(){
     getSurvivors(this);
-    
   }
 
   render(){
-    let survivors = this.state.survivors
+    getSurvivorsId(this);
+    let survivors = this.state.survivors;
+    let rows = [];
+    survivors.map((survivor) => {
+      if(!survivor["infected?"]){
+        rows.push(<SurvivorsRow survivor={survivor} key={survivor["id"]} />);
+      }
+    });
+
     return(
-        <ul className="survivors-list">
-          <table>
-            <thead>
-            <tr>
-              <th>Name</th>
-              <th>Gender</th>
-              <th>Age</th>
-              <th>Lat</th>
-              <th>Long</th>
-              <th>Water</th>
-              <th>Food</th>
-              <th>Meds</th>
-              <th>Ammo</th>
-              <th colSpan="0"></th>
-            </tr>
-          </thead>
-          <tbody>
-            {survivors.map(survivor =>
-              <Survivor key={getSurvivorId(survivor["location"])} survivor={survivor} />
-            )}
-          </tbody>
-        </table>
-      </ul>
-    )
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Gender</th>
+            <th>Age</th>
+            <th>Lat</th>
+            <th>Long</th>
+            <th>Water</th>
+            <th>Food</th>
+            <th>Meds</th>
+            <th>Ammo</th>
+            <th colSpan="0"></th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    );
   }
 }
 
-function getSurvivors (e){
-  return fetch('http://zssn-backend-example.herokuapp.com/api/people.json')
-  .then((response) => response.json())
-  .then((survivors) => {
-    e.setState({survivors})
-  })
+class SurvivorsRow extends React.Component{
+  render(){
+    return(
+      <tr>
+        <td>{this.props.survivor["name"]}</td>
+        <td>{this.props.survivor["gender"]}</td>
+        <td>{this.props.survivor["age"]}</td>
+        <td>{this.props.survivor["lonlat"]}</td>
+        <td>{this.props.survivor["lonlat"]}</td>
+        <td>x</td>
+        <td>x</td>
+        <td>x</td>
+        <td>x</td>
+      </tr>
+    );
+  }
 }
-
-function getSurvivorId(location){
-  return location.split("people/")[1]
-}
-
-
-const Survivor = (props) => (
-  <tr>
-    <td>{props.survivor["name"]}</td>
-    <td>{props.survivor["gender"]}</td>
-    <td>{props.survivor["age"]}</td>
-    <td>{props.survivor["lonlat"]}</td>
-    <td>{props.survivor["lonlat"]}</td>
-    <td>x</td>
-    <td>x</td>
-    <td>x</td>
-    <td>x</td>
-  </tr>
-)
-
 export default Survivors;
